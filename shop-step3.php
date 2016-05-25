d54f2d5defb00dc234451c18e68ce23de700a9aa
Vanvolodya <Vanvolodya@gmail.com>
2016-05-26 00:31:03 +0300

    Update shop-step3.php

--- shop-step3.php
+++ shop-step3.php
@@ -2,9 +2,9 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
-$outp = '[ {"name":"Ноутбук Lenovo IdeaPad 100-14", "icon":"lenovoIdeaPad10014.jpg", "desc":"Екран 14 (1366x768) HD LED, глянцевий / Intel Celeron N2840 (2.16 - 2.58 ГГц) / RAM 2 ГБ / HDD 500 ГБ / Intel HD Graphics / LAN / Wi-Fi / веб-камера / DOS / 1.9 кг / чорний", "price":5999},'.
-			'{"name":"Ноутбук HP 255 G4", "icon":"HP255G4.jpg", "desc":"Екран 15.6 (1366x768) HD LED, матовий / AMD Dual-Core E1-6015 (1.4 ГГц) / RAM 2 ГБ / HDD 500 ГБ / AMD Radeon R2 / LAN / Wi-Fi / Bluetooth / веб-камера / Linux / 2.15 кг / чорний", "price":6099},'.
-			'{"name":"Ноутбук Dell Inspiron 3542", "icon":"DellInspiron3542.jpg", "desc":"Екран 15.6 (1366x768) WXGA HD LED, глянцевий / Intel Celeron 2957U (1.4 ГГц) / RAM 2 ГБ / HDD 500 ГБ / Intel HD Graphics / DVD Super Multi / LAN / Wi-Fi / Bluetooth 4.0 / веб-камера / Linux / 2.4 кг / чорний", "price":6777} ]';
+$outp = '[ {"name":"Мяч для футболу SELECT Contra", "icon":"b1.jpg", "desc":"Ручний шов; мяч обладнано запатентованою компанією SELECT камерою Zero-Wing з подвійним бутильовим клапаном, який забезпечує стабільний баланс мяча в польоті; призначений для офіційних ігор і тренувань клубних команд; випускається в рамках CLUB SERIES (мячі для клубів); дуже міцний; мяч дуже легкий у прийомі та управлінні", "price":598},'.
+			'{"name":"Мяч для футболу K-Sector Pro Team", "icon":"b2.jpg", "desc":"", "price":403},'.
+			'{"name":"Мяч для футболу NIKE PREMIER TEAM FIFA", "icon":"b5.jpg", "desc":"Мяч рекомендований для ігор і тренувань професійних команд і клубів; ламіноване покриття PU, яке харктерізуеться низьким водопоглинанням і високою стійкістю до зносу; панелі зроблені зі спеціального поліуретанового волокна, схожого на піну, розробленого компанією Nike, що надає мячу відмінну мякість, він довше зберігає форму", "price":907} ]';
 echo($outp);
 ?>
 
